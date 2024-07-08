91eec124e7fe61783fed16129d35a7c40f3fac46
Fernando Carvalho <68618354+21fernandinhow@users.noreply.github.com>
2024-07-08 10:39:37 -0300

    Fix issue on date convert

--- src/components/CreateTimeMessage.tsx
+++ src/components/CreateTimeMessage.tsx
@@ -21,7 +21,7 @@ export default function CreateTimeMessage(props: CreateTimeMessageProps) {
         const params = {
             time_message: {
                 content: newMessageContent,
-                date_to_open: newMessageDate.split("/").reverse.join("-")
+                date_to_open: newMessageDate.split("/").reverse().join("-")
             }
         }
         await api.post(`/users/${props.user}/time_messages`, params)
