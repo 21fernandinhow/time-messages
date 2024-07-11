45e64e84946046f70c9d23ac09f2144045a4dbfd
21fernandinhow <21fernandinhow@gmail.com>
2024-07-11 19:41:32 -0300

    footer link now open on a new tab

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,12 @@ export default function Footer () {
         <footer className="footer footer-center bg-base-100 text-base-content p-4">
             <aside>
                 <p>Desenvolvido com <strong className="text-neutral">♥</strong> por 
-                <a 
-                    className="text-neutral font-bold mx-1 hover:underline" 
-                    href="https://fernandocarvalhodev.com">
+                    <a 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                        className="text-neutral font-bold mx-1 hover:underline" 
+                        href="https://fernandocarvalhodev.com"
+                    >
                         Fernando Carvalho
                     </a>
                 </p>
